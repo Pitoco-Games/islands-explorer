8afedaa169915d78306365c0325f70630622bf62
Bernardo Hörner <bernardo.horner@tapps.com.br>
2022-11-04 01:17:21 -0300

    Add new parameters for the player movement scriptable object

--- Islands Explorer/Assets/Scripts/Player controls/Movement/MovementStatsSO.cs
+++ Islands Explorer/Assets/Scripts/Player controls/Movement/MovementStatsSO.cs
@@ -6,12 +6,17 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObject/Player stats/Movement")]
 public class MovementStatsSO : ScriptableObject
 {
+   [Header("Ground Movement")]
    public float MaxVelocity;
    public float Acceleration;
    public float Deceleration;
    public float TurnSpeed;
    public float JumpStrength;
-   public float Gravity;
    public float MovementAngleLimitToReverse;
    public float ReverseDeceleration;
+   public float MaxSlopeAngle;
+
+   [Header("Jump")]
+   public float FallSpeedMultiplier;
+   public float Gravity;
 }
